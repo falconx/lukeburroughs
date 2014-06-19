e572290044f36c98fddf2cb5e6612c5b5a6427ff
Matt Layton <mattlayt@gmail.com>
2014-06-19 14:10:46 +0100

    Update

--- app.js
+++ app.js
@@ -14,13 +14,13 @@ app.engine('html', require('ejs').renderFile);
 app.set('port', process.env.PORT || 5000);
 app.set('view engine', 'html');
 
-app.use(express.static(path.join(__dirname, 'public')));
+app.use('/public', express.static(path.join(__dirname, 'public')));
 
 /**
  * Route all traffic to angular directory for further routing
  */
 
-// app.get('*', function( req, res ) {
+// app.get('/', function( req, res ) {
 // 	res.redirect('index');
 // });
 
