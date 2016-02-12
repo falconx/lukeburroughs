baf24196a472b88bed1fe0eebfe66704d1f1c054
Matt Layton <mattlayt@gmail.com>
2016-02-12 23:11:29 +0000

    Update

--- gulpfile.js
+++ gulpfile.js
@@ -34,29 +34,29 @@ var paths = {
 gulp.task('default', ['compress']);
 
 gulp.task('compress', ['scripts', 'assets', 'css'], function() {
-	gutil.log( gutil.colors.green('Files compressed') );
+	gutil.log(gutil.colors.green('Files compressed'));
 });
 
 // Clean out build directory
 
 gulp.task('clean', function() {  
-	return gulp.src( paths.buildDir, { read: false }).pipe( clean() );
+	return gulp.src(paths.buildDir, { read: false }).pipe(clean());
 });
 
 // Watch for changes
 
 gulp.task('watch', function() {
-	gutil.log( 'gulp:watch task running...' );
+	gutil.log('gulp:watch task running...');
 
-	gulp.watch( paths.scripts, ['scripts'] );
-	gulp.watch( paths.assets, ['assets'] );
-	gulp.watch( paths.stylesheets, ['css'] );
+	gulp.watch(paths.scripts, ['scripts']);
+	gulp.watch(paths.assets, ['assets']);
+	gulp.watch(paths.stylesheets, ['css']);
 });
 
 // Lint scripts
 
 gulp.task('lint', function() {
-	gulp.src( 'scripts/app.js' )
+	gulp.src('scripts/app.js')
 		.pipe(jshint())
 		.pipe(jshint.reporter('jshint-summary', {
 			verbose: true,
@@ -68,33 +68,33 @@ gulp.task('lint', function() {
 // Minify scripts
 
 gulp.task('scripts', ['lint'], function() {
-	gulp.src( paths.scripts )
-		.pipe( uglify() )
-		.pipe( concat('lukeburroughs.min.js') )
-		.pipe( gulp.dest( paths.buildDir ) );
+	gulp.src(paths.scripts)
+		.pipe(uglify())
+		.pipe(concat('lukeburroughs.min.js'))
+		.pipe(gulp.dest(paths.buildDir));
 });
 
 // Minify image assets and maintain file structure by setting base
 // Todo: PNG's
 
 gulp.task('assets', function() {
-	return gulp.src( paths.assets, { base: 'public' } )
+	return gulp.src(paths.assets, { base: 'public' })
 		// Only compress files which have been modified
-		.pipe(changed( paths.buildDir ))
+		.pipe(changed(paths.buildDir))
 			.pipe(imagemin({
 				progressive: true,
 				svgoPlugins: [{
-				removeViewBox: false
-			}]
-		}))
-		.pipe( gulp.dest( paths.buildDir ) );
+					removeViewBox: false
+				}]
+			}))
+			.pipe(gulp.dest(paths.buildDir));
 });
 
 // Minify stylesheets
 
 gulp.task('css', function() {
-	gulp.src( paths.stylesheets )
-		.pipe( cssmin() )
-		.pipe( concat('lukeburroughs.min.css') )
-		.pipe( gulp.dest( paths.buildDir ) );
+	gulp.src(paths.stylesheets)
+		.pipe(cssmin())
+		.pipe(concat('lukeburroughs.min.css'))
+		.pipe(gulp.dest(paths.buildDir));
 });
